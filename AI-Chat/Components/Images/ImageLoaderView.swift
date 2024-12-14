7cb1e6fc5f5fb86bf26ddc7af92946b6bbefcbcb
Soham Divekar <codingguy14@gmail.com>
2024-12-15 10:49:33 +1100

    [Feature] Add WelcomeView UI

---
+++ AI-Chat/Components/Images/ImageLoaderView.swift
@@ -0,0 +1,32 @@
+//
+//  ImageLoaderView.swift
+//  AI-Chat
+//
+//  Created by Soham Divekar on 15/12/2024.
+//
+
+import SwiftUI
+import SDWebImageSwiftUI
+
+struct ImageLoaderView: View {
+    
+    var urlString: String = Constants.randomImage
+    var resizingMode: ContentMode = .fill
+    
+    var body: some View {
+        Rectangle()
+            .opacity(0)
+            .overlay {
+                WebImage(url: URL(string: urlString))
+                    .resizable()
+                    .indicator(.activity)
+                    .aspectRatio(contentMode: resizingMode)
+                    .allowsHitTesting(false)
+            }
+            .clipped()
+    }
+}
+
+#Preview {
+    ImageLoaderView()
+}
---
+++ AI-Chat/Utilities/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  AI-Chat
+//
+//  Created by Soham Divekar on 15/12/2024.
+//
+
+import Foundation
+
+struct Constants {
+    static let randomImage = "https://picsum.photos/600/600"
+    static let privacyPolicyUrl = "https://www.apple.com"
+    static let termsOfServiceUrl = "https://www.apple.com"
+}
