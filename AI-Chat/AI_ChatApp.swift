cfc593b5ee720c67592d4f32d98e5f80b1534cbc
Soham Divekar <codingguy14@gmail.com>
2024-12-14 16:02:07 +1100

    Initial Commit

---
+++ AI-Chat/AI_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_ChatApp.swift
+//  AI-Chat
+//
+//  Created by Soham Divekar on 14/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct AI_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AI-ChatTests/AI_ChatTests.swift
@@ -0,0 +1,16 @@
+//
+//  AI_ChatTests.swift
+//  AI-ChatTests
+//
+//  Created by Soham Divekar on 14/12/2024.
+//
+
+import Testing
+
+struct AI_ChatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
